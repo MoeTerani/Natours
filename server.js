26becb659ed92782c82a453ca54dbe6b68272b04
MoeTerani <moe.terani@gmail.com>
2019-09-23 10:10:16 +0200

    created a new tour schema and then a model based on that schema

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ mongoose
     console.log('DB connection successfu!');
   });
 
+// set up the mongoose schema
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Error: A tour must have a name'] // this is called a validator, validate if the data is there and throw an error if NOT.
+  },
+  price: {
+    type: Number,
+    required: [true, 'Error: A tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+// create a Tour model out of the tour schema
+const Tour = mongoose.model('Tour', tourSchema); // convention to capoitilixe the name of the models and variables
+
 const app = require('./app');
 
 // ---------start the server------------
