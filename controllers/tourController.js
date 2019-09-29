4262d678591ac8af4cb2f1cc9421d0a99df1b3e4
Moe Terani <moe.terani@gmail.com>
2019-09-29 21:59:59 +0200

    getAllTours and getTour async functions connected to database and set fetch data by find89 and findById()

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,21 +12,41 @@ const tours = JSON.parse(
 // if not , send back a 400 bad reequest message.admin-nav
 //ADD to post handler stack to
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestesAt: req.requestTime
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-    /*   results: tours.length, // whenever sending an Array to client , would be helpful to send the lenght of the array so we know how many obj we Are recieving.
+    res.status(200).json({
+      status: 'success',
+      data: { tours: tours }
+
+      /*   results: tours.length, // whenever sending an Array to client , would be helpful to send the lenght of the array so we know how many obj we Are recieving.
     data: { tours: tours } //first tours is same as the URL tours and the secong one is the variable that we want to send as respose
    */
-  });
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'faill',
+      message: err
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params); // information about the id is stored in req.params
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    //finfById() = Tour.findOne({_id: req.params.id})
 
+    res.status(200).json({
+      status: 'success',
+      data: { tour: tour }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'faill',
+      message: err
+    });
+  }
   const id = req.params.id * 1; // Nice trick to turn string into number inside java script
 
   /*   const tour = tours.find(tour => tour.id === id); // use find method to return a new first object with the same id
